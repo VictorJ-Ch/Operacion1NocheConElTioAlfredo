3ad5016b99302860bd8925f7771a7818d9da650b
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2024-03-03 22:16:44 -0600

    Linterna
    Se prende con click izquierdo y se apaga con el mismo

---
+++ Assets/asset/Flashlight/Linterna.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour
+{
+    public Light Luz;
+
+    private void Start()
+    {
+        Luz.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Luz.enabled == false)
+            {
+                Luz.enabled = true;
+            }
+
+            else if (Luz.enabled == true)
+            {
+                Luz.enabled = false;
+            }
+        }
+    }
+}
