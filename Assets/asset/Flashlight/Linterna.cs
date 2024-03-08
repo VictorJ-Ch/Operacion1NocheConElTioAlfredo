db277646972015a0f675438c8980f381ac26eff8
Kaseem <chavarria.martinez.victor101@gmail.com>
2024-03-08 08:09:18 -0600

    Flashlight
    - Panic Scene
    + Flashlight Script

--- Assets/asset/Flashlight/Linterna.cs
+++ Assets/asset/Flashlight/Linterna.cs
@@ -7,21 +7,42 @@ public class Linterna : MonoBehaviour
 {
     public Light Luz;
 
+    public float Pila;
+    public float PilaRestante;
+    float PilaMax;
+    public float PotenciaMin;
+
     private void Start()
     {
         Luz.enabled = false;
+        PilaRestante = Pila;
+        PilaMax = Pila;
     }
 
     private void Update()
     {
         Prende_Apaga();
+
+        if (PilaRestante <= 0)
+        {
+            Luz.enabled = false;
+        }
+
+        if (Luz.enabled == true && PilaRestante >= 0)
+        {
+            PilaRestante -= Time.deltaTime;
+        }
+        else if(Luz.enabled == false && PilaRestante <= PilaMax)
+        {
+            PilaRestante += Time.deltaTime;
+        }
     }
 
     void Prende_Apaga()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (Luz.enabled == false)
+            if (Luz.enabled == false && PilaRestante >= PotenciaMin)
             {
                 Luz.enabled = true;
             }
@@ -30,6 +51,8 @@ public class Linterna : MonoBehaviour
             {
                 Luz.enabled = false;
             }
+
+            
         }
     }
 }
